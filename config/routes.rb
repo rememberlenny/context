b74dc6edcbd7cfe6bf31aeed7c7d85b77189484a
rememberlenny <rememberlenny@gmail.com>
2015-05-21 12:57:25 -0400

    Update the urls

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,16 @@ Rails.application.routes.draw do
   root 'topics#featured'
 
   get 't/:id' => 'topics#show'
-  resources :topics, as: 't' do
-    resources :stories, only: [:show, :edit, :update, :destroy], as: 's'
-  end
-  resources :stories, only: [:index, :new, :create], as: 's'
+    resources :topics, :path => '/t' do
+      resources :stories, only: [:show, :edit, :update, :destroy], :path => '/s'
+    end
+  resources :stories, only: [:index, :new, :create], :path => '/s'
 
   get 'u/:id' => 'users#show'
 
 
   # root 'landing#no_info'
   match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
-  devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks' }
+  devise_for :users, :path => '/u', :controllers => { omniauth_callbacks: 'omniauth_callbacks' }
 
 end
