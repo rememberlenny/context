0366c369ba73c59c97a82ca08a8d5db2571db646
rememberlenny <rememberlenny@gmail.com>
2015-05-21 14:26:28 -0400

    Create the need for validate

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,6 +1,8 @@
 class Story < ActiveRecord::Base
   has_one :topic
+
   belongs_to :user, dependent: :destroy
+  validates :topic_id, presence: true
   validates :name, presence: true,
                      length: { maximum: 255 },
                  uniqueness: { case_sensitive: false }
