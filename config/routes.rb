52f9b62458403d72b8810a0e9c1f8c713e42ebfc
rememberlenny <rememberlenny@gmail.com>
2015-05-27 19:48:06 -0400

    Fix user

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,13 @@ Rails.application.routes.draw do
   resources :topics, :path => '/t'
   resources :stories, :path => '/s'
 
-  get 'u/:id' => 'users#show'
 
 
   # root 'landing#no_info'
-  match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
-  devise_for :users, :path => '/u', :controllers => { omniauth_callbacks: 'omniauth_callbacks' }
+  match '/u/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
+  devise_for :users, :path => '/u', :controllers => {
+    omniauth_callbacks: 'omniauth_callbacks'
+  }
+  get 'u/:id' => 'users#show'
 
 end
